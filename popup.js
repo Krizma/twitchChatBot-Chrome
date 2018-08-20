f7e7403c5e6376b239f1f8877ebb648d8d412efd
Krizma <antikrizma@gmail.com>
2018-08-20 14:36:02 -0500

    8/19 Update
    This is still 0.1a as the code has not been finished yet, still working on the best way to enter items into chat. The sending process is complete though.

--- popup.js
+++ popup.js
@@ -7,19 +7,42 @@ document.addEventListener('DOMContentLoaded', function() {
         if (img.indexOf('off_128.png')!=-1) {
             document.getElementById('01').src  = 'images/on_128.png';
             
-            function prep() {
+            /*function prep() {
                 setTimeout(function() {console.log("wait 10 seconds ")}, 1000);
+            } */
+            var array = ["gmohnDab", "garzDab", "steelKappa", "PepePls", "Jebaited", "BootyTime", "POGGERS", "LuL", "garzHaHAA" , "steelREALLY", "steelO", "garzOH", "ZreknarF", "BibleThump", "steelSalt", "steelGrin", "steelWOO", "steelDank", "garzW", "TTours", "KappaPride", "riPepperonis", "TriHard", "KKomrade", "steel4"];
+            //console.log(array);
+            setInterval(function(){
+            var rand = Math.floor(Math.random() * 24);
+            function randomWholeNum() {
+            // Only change code below this line.
+            return Math.floor(Math.random() * 10);
             }
-            function bot() {
-                var array = ["gmohnDab", "garzDab", "steelKappa", "PepePls", "Jebaited", "BootyTime", "POGGERS", "LuL", "garzHaHAA" , "steelREALLY", "steelO", "garzOH", "ZreknarF", "BibleThump", "steelSalt", "steelGrin", "steelWOO", "steelDank", "garzW", "TTours", "KappaPride", "riPepperonis", "TriHard", "KKomrade", "steel4"];
-                console.log(array);
+            console.log(rand);
+            var emote = array[rand];
+            console.log(emote);
+            // document.getElementsByClassName("tw-block tw-border-radius-medium tw-font-size-6 tw-full-width tw-textarea tw-textarea--no-resize")
+            var text = emote;
+            function sendInput() {
+                var buttonValue
+            }
 
-
-
-                }
-            }        
+            var e = new Event("keydown");
+            e.key="return";    // just enter the char you want to send 
+            e.keyCode=e.key.charCodeAt(13);
+            e.which=e.keyCode;
+            e.altKey=false;
+            e.ctrlKey=true;
+            e.shiftKey=false;
+            e.metaKey=false;
+            e.bubbles=true;
+            document.dispatchEvent(e);
+            console.log(e);
+            }, 5000);
+}                
          else {
            document.getElementById('01').src = 'images/off_128.png';
+           clearInterval();
        }
 
     })
